253e0225345dbbf3ddd7c708979b8fe3f2f15558
fuzhanlun <fuzhanlun@126.com>
2020-05-30 23:02:54 +0800

    完善使用教程

--- demo/app.js
+++ demo/app.js
@@ -6,4 +6,9 @@ const FastServe = require('./fastserve');
 FastServe.config.cache  = false; //取消模板缓存
 FastServe.config.maxage = 0; //取消静态文件缓存
 
+//响应get请求
+FastServe.get('/hello', (req, res) => {
+	res.html('hello world!');
+});
+
 FastServe.createServer().listen(80);
